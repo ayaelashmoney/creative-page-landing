c00a36e796081df291841fbc239b2384a216d4a3
ayaelashmoney <ayaelashmoney123@gmail.com>
2024-12-25 15:12:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let imag=document.querySelector('.imag');
+let container=document.querySelector('.container');
+function phones(phone){
+    imag.src=phone;
+}
+function colors(color){
+    container.style.background=color;
+}
